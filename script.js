9218c3a8c5b5dd45fc62cc4a595be2dd1d9a6c34
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-02-23 00:34:51 +0330

    remove duplicated codes

--- script.js
+++ script.js
@@ -38,13 +38,11 @@ setInterval(() => {
   nextSlide(1)
 }, 3000);
 
-// Create Quiz
-
 const questionCounter = document.querySelector('#question_counter')
 const numberError = document.querySelector('#number_error')
 const questionAnswerDiv = document.querySelector('.question_answer')
-const quizesDiv = document.querySelector('.quizes')
-const quizToTake = document.querySelector('.quiz_to_take')
+const quizCardsDiv = document.querySelector('.quizes')
+const quizToTakeDiv = document.querySelector('.quiz_to_take')
 
 class Quiz {
   constructor(questions) {
@@ -62,53 +60,62 @@ class Question {
   }
 }
 
-function renderPreviousQuestion(event) {
+function removeAllChildren(parentDiv) {
+  while(parentDiv.firstChild) {
+    parentDiv.removeChild(parentDiv.firstChild);
+  }
+}
+
+// Create Quiz
+
+function saveQuiz() {
+  const newQuiz = new Quiz(questions)
+  quizes.push(newQuiz)
+  localStorage.setItem('Quiz Bank', JSON.stringify(quizes))
+
+  questionIndex = 0
+  localStorage.removeItem('Current Quiz')
+
+  const createQuizDiv = document.querySelector('#create_quiz_container')
+  removeAllChildren(createQuizDiv)
+
+  renderQuizCards()
+}
+
+function changeQuestionToCreate(questionIndex) {
+  const currentQuiz = JSON.parse(localStorage.getItem('Current Quiz'))
+
+  if (currentQuiz && currentQuiz[questionIndex]) {
+    const {text, choices, answerIndex} = currentQuiz[questionIndex]
+    document.querySelector('#question_box').value = text
+    for (let i = 1; i <= numOfChoices; i++) {
+      document.querySelector(`.choice${i}`).value = choices[i - 1]
+    }
+    document.querySelector('.correct_choice').value = answerIndex + 1
+  } else {
+    document.querySelector('#question_box').value = ""
+    for (let i = 1; i <= numOfChoices; i++) {
+      document.querySelector(`.choice${i}`).value = ""
+    }
+    document.querySelector('.correct_choice').value = ""
+  }
+}
+
+function createPreviousQuestion(event) {
   event.preventDefault()
 
   if (questionIndex > 0) {
     questionIndex--;
-
-    const currentQuiz = JSON.parse(localStorage.getItem('Current Quiz'))
-
-    if (currentQuiz && currentQuiz[questionIndex]) {
-      const {text, choices, answerIndex} = currentQuiz[questionIndex]
-      document.querySelector('#question_box').value = text
-      for (let i = 1; i <= numOfChoices; i++) {
-        document.querySelector(`.choice${i}`).value = choices[i - 1]
-      }
-      document.querySelector('.correct_choice').value = answerIndex + 1
-    } else {
-      document.querySelector('#question_box').value = ""
-      for (let i = 1; i <= numOfChoices; i++) {
-        document.querySelector(`.choice${i}`).value = ""
-      }
-      document.querySelector('.correct_choice').value = ""
-    }
+    changeQuestionToCreate(questionIndex)
   }
 }
 
-function renderNextQuestion(event) {
+function createNextQuestion(event) {
   event.preventDefault()
 
   if (questionIndex < questionCounter.value - 1) {
     questionIndex++;
-
-    const currentQuiz = JSON.parse(localStorage.getItem('Current Quiz'))
-
-    if (currentQuiz && currentQuiz[questionIndex]) {
-      const {text, choices, answerIndex} = currentQuiz[questionIndex]
-      document.querySelector('#question_box').value = text
-      for (let i = 1; i <= 4; i++) {
-        document.querySelector(`.choice${i}`).value = choices[i - 1]
-      }
-      document.querySelector('.correct_choice').value = answerIndex + 1
-    } else {
-      document.querySelector('#question_box').value = ""
-      for (let i = 1; i <= numOfChoices; i++) {
-        document.querySelector(`.choice${i}`).value = ""
-      }
-      document.querySelector('.correct_choice').value = ""
-    }
+    changeQuestionToCreate(questionIndex)
   }
 }
 
@@ -156,219 +163,12 @@ function saveQuestion(event) {
   }
 }
 
-function saveQuiz() {
-  const newQuiz = new Quiz(questions)
-  quizes.push(newQuiz)
-  questionIndex = 0
-  localStorage.setItem('Quiz Bank', JSON.stringify(quizes))
-  localStorage.removeItem('Current Quiz')
-
-  const createQuizDiv = document.querySelector('#create_quiz_container')
-  while(createQuizDiv.firstChild) {
-    createQuizDiv.removeChild(createQuizDiv.firstChild)
-  }
-
-  renderQuizesSection()
-}
-
-function takeNextQuestion(event) {
-  event.preventDefault()
-
-  updateScore(questionIndex)
-
-  const { questions } = quizes[quizIndex]
-
-  if (questionIndex < questions.length - 1) {
-    questionIndex++;
-
-    const questionBox = document.querySelector('.questions')
-    questionBox.innerText = `${questionIndex + 1}. ${questions[questionIndex].text}` // sth
-
-    const choicesDiv = document.querySelector(`.choices`)
-    while(choicesDiv.firstChild) {
-      choicesDiv.removeChild(choicesDiv.firstChild)
-    }
-    for (let i = 1; i <= numOfChoices; i++) {
-      choicesDiv.innerHTML += `
-      <div>
-        <input type="radio" name="choice" id="choice${i}">
-        <label>${questions[questionIndex].choices[i - 1]}</label>
-      </div>
-      `
-    }
-    updateRadioButtons(questionIndex)
-  }
-}
-
-function finishTakingQuiz(event) {
-  event.preventDefault()
-
-  updateScore(questionIndex)
-  calculateScore()
-
-  while(quizToTake.firstChild) {
-    quizToTake.removeChild(quizToTake.firstChild)
-  }
-
-  const scoreBoard = document.createElement('h4')
-  scoreBoard.innerText = ` Your score is ${userScore} of ${userAnswers.length}`
-  quizToTake.appendChild(scoreBoard)
-
-  // Hide taken quiz
-}
-
-function calculateScore() {
-  const { questions } = quizes[quizIndex]
-  for (let i = 0; i < userAnswers.length; i++) {
-    if (userAnswers[i] == questions[i].answerIndex) {
-      userScore++
-    }
-  }
-}
-
-function takePreviousQuestion(event) {
-  event.preventDefault()
-
-  updateScore(questionIndex)
-
-  const { questions } = quizes[quizIndex]
-
-  if (questionIndex > 0) {
-    questionIndex--;
-
-    const questionBox = document.querySelector('.questions')
-    questionBox.innerText = `${questionIndex + 1}. ${questions[questionIndex].text}` // sth
-
-    const choicesDiv = document.querySelector(`.choices`)
-    while(choicesDiv.firstChild) {
-      choicesDiv.removeChild(choicesDiv.firstChild)
-    }
-    for (let i = 1; i <= numOfChoices; i++) {
-      choicesDiv.innerHTML += `
-      <div>
-        <input type="radio" name="choice" id="choice${i}">
-        <label>${questions[questionIndex].choices[i - 1]}</label>
-      </div>
-      `
-    }
-    updateRadioButtons(questionIndex)
-  }
-}
-
-function updateRadioButtons(questionIndex) {
-  const userAnswer = userAnswers[questionIndex]
-  if (userAnswer != -1) {
-    document.getElementById(`choice${userAnswer + 1}`).checked = true
-  }
-}
-
-function updateScore(questionIndex) {
-  if (document.querySelector('#choice1').checked) {
-    userAnswers[questionIndex] = 0
-  } else if (document.querySelector('#choice2').checked) {
-    userAnswers[questionIndex] = 1
-  } else if (document.querySelector('#choice3').checked) {
-    userAnswers[questionIndex] = 2
-  } else if (document.querySelector('#choice4').checked) {
-    userAnswers[questionIndex] = 3
-  }
-}
-
-function takeQuiz(quizIndexToTake) {
-
-  quizIndex = quizIndexToTake
-  const questions = quizes[quizIndex].questions
-  questionIndex = 0
-  const question = questions[questionIndex]
-
-  for (let i = 0; i < questions.length; i++) {
-    userAnswers.push(-1)
-  }
-
-  quizToTake.innerHTML += `
-    <div class="col">
-      <a href="#">
-        <button class="previous_button">
-          Previous
-        </button>
-      </a>
-      <a href="#">
-        <button class="next_button">
-          Next
-        </button>
-      </a>
-      <a href="#">
-        <button class="finish_button">
-          Finish
-        </button>
-      </a>
-    </div>
-    <div class="qa_to_take">
-      <div class="questions">
-        ${1}. ${question.text}
-      </div>
-      <div class="choices">
-        <div>
-          <input type="radio" name="choice" id="choice1">
-          <label>${question.choices[0]}</label>
-        </div>
-        <div>
-          <input type="radio" name="choice" id="choice2">
-          <label>${question.choices[1]}</label>
-        </div>
-        <div>
-          <input type="radio" name="choice" id="choice3">
-          <label>${question.choices[2]}</label>
-        </div>
-        <div>
-          <input type="radio" name="choice" id="choice4">
-          <label>${question.choices[3]}</label>
-        </div>
-      </div>
-    </div>
-  `
-
-  const nextBtn = document.querySelector('.col .next_button')
-  const finishBtn = document.querySelector('.col .finish_button')
-  const previousBtn = document.querySelector('.col .previous_button')
-
-  nextBtn.addEventListener('click', takeNextQuestion)
-  finishBtn.addEventListener('click', finishTakingQuiz)
-  previousBtn.addEventListener('click', takePreviousQuestion)
-}
-
-function renderQuizesSection() {
-  while(quizesDiv.firstChild) {
-    quizesDiv.removeChild(quizesDiv.firstChild)
-  }
-
-  const quizes = JSON.parse(localStorage.getItem('Quiz Bank'))
-  quizes ?
-    quizes.map(quiz => {
-      quizesDiv.innerHTML += `
-        <div class="quiz_card">
-          <div class="box">
-            <div class="content">
-              <h2>${quiz.index + 1}</h2>
-              <h3>Quiz ${quiz.index + 1}</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
-              <a href="#" onclick="takeQuiz(${quiz.index})">Take this quiz</a>
-            </div>
-          </div>
-        </div>
-      `
-    }) :
-    quizesDiv.innerHTML += `<p>There is no available quiz to take now!</p>`
-}
-
 function createQuiz() {
-  if (questionCounter.value > 0) {
+  if(questionCounter.value > 0) {
     numOfQuestions = questionCounter.value;
-    while (questionAnswerDiv.firstChild) {
-      questionAnswerDiv.removeChild(questionAnswerDiv.firstChild);
-    }
 
-    // render question and answer fields to fill them out
+    removeAllChildren(questionAnswerDiv)
+
     questionAnswerDiv.innerHTML += `
     <div class="quiz_creation">
       <div>
@@ -411,16 +211,193 @@ function createQuiz() {
     const saveBtn = document.querySelector('.save_button')
     const previousBtn = document.querySelector('.previous_button')
 
-    nextBtn.addEventListener('click', renderNextQuestion)
+    nextBtn.addEventListener('click', createNextQuestion)
     saveBtn.addEventListener('click', saveQuestion)
-    previousBtn.addEventListener('click', renderPreviousQuestion)
+    previousBtn.addEventListener('click', createPreviousQuestion)
 
   } else {
     numberError.innerText = "Number of questions must be more than 0 !"
   }
 }
 
+// Take Quiz
 
+function calculateQuizScore() {
+  const { questions } = quizes[quizIndex]
+  for (let i = 0; i < userAnswers.length; i++) {
+    if (userAnswers[i] == questions[i].answerIndex) {
+      userScore++
+    }
+  }
+}
+
+function updateRadioButtons(questionIndex) {
+  const userAnswer = userAnswers[questionIndex]
+  if (userAnswer != -1) {
+    document.getElementById(`choice${userAnswer + 1}`).checked = true
+  }
+}
+
+function changeQuestionToTake(questions, questionIndex) {
+  const questionBox = document.querySelector('.questions')
+  questionBox.innerText = `${questionIndex + 1}. ${questions[questionIndex].text}`
+
+  const choicesDiv = document.querySelector(`.choices`)
+  removeAllChildren(choicesDiv)
+
+  for (let i = 1; i <= numOfChoices; i++) {
+    choicesDiv.innerHTML += `
+    <div>
+      <input type="radio" name="choice" id="choice${i}">
+      <label>${questions[questionIndex].choices[i - 1]}</label>
+    </div>
+    `
+  }
+
+  updateRadioButtons(questionIndex)
+}
+
+function updateScore(questionIndex) {
+  if (document.querySelector('#choice1').checked) {
+    userAnswers[questionIndex] = 0
+  } else if (document.querySelector('#choice2').checked) {
+    userAnswers[questionIndex] = 1
+  } else if (document.querySelector('#choice3').checked) {
+    userAnswers[questionIndex] = 2
+  } else if (document.querySelector('#choice4').checked) {
+    userAnswers[questionIndex] = 3
+  }
+}
+
+function takeNextQuestion(event) {
+  event.preventDefault()
+
+  updateScore(questionIndex)
+
+  const { questions } = quizes[quizIndex]
+
+  if (questionIndex < questions.length - 1) {
+    questionIndex++;
+    changeQuestionToTake(questions, questionIndex)
+  }
+}
+
+function takePreviousQuestion(event) {
+  event.preventDefault()
+
+  updateScore(questionIndex)
+
+  const { questions } = quizes[quizIndex]
+
+  if (questionIndex > 0) {
+    questionIndex--;
+    changeQuestionToTake(questions, questionIndex)
+  }
+}
+
+function finishTakingQuiz(event) {
+  event.preventDefault()
+
+  updateScore(questionIndex)
+  calculateQuizScore()
+
+  removeAllChildren(quizToTakeDiv)
+
+  const scoreBoard = document.createElement('h4')
+  scoreBoard.innerText = ` Your score is ${userScore} of ${userAnswers.length}`
+  quizToTakeDiv.appendChild(scoreBoard)
+}
+
+function renderQuizToTake(firstQuestion) {
+  quizToTakeDiv.innerHTML += `
+    <div class="col">
+      <a href="#">
+        <button class="previous_button">
+          Previous
+        </button>
+      </a>
+      <a href="#">
+        <button class="next_button">
+          Next
+        </button>
+      </a>
+      <a href="#">
+        <button class="finish_button">
+          Finish
+        </button>
+      </a>
+    </div>
+    <div class="qa_to_take">
+      <div class="questions">
+        ${1}. ${firstQuestion.text}
+      </div>
+      <div class="choices">
+        <div>
+          <input type="radio" name="choice" id="choice1">
+          <label>${firstQuestion.choices[0]}</label>
+        </div>
+        <div>
+          <input type="radio" name="choice" id="choice2">
+          <label>${firstQuestion.choices[1]}</label>
+        </div>
+        <div>
+          <input type="radio" name="choice" id="choice3">
+          <label>${firstQuestion.choices[2]}</label>
+        </div>
+        <div>
+          <input type="radio" name="choice" id="choice4">
+          <label>${firstQuestion.choices[3]}</label>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+function takeQuiz(quizIndexToTake) {
+
+  quizIndex = quizIndexToTake
+  const questions = quizes[quizIndex].questions
+  questionIndex = 0
+  const question = questions[questionIndex]
+
+  renderQuizToTake(question)
+
+  for (let i = 0; i < questions.length; i++) {
+    userAnswers.push(-1)
+  }
+
+  const nextBtn = document.querySelector('.col .next_button')
+  const finishBtn = document.querySelector('.col .finish_button')
+  const previousBtn = document.querySelector('.col .previous_button')
+
+  nextBtn.addEventListener('click', takeNextQuestion)
+  finishBtn.addEventListener('click', finishTakingQuiz)
+  previousBtn.addEventListener('click', takePreviousQuestion)
+}
+
+// Render Created Quizes to Take
+
+function renderQuizCards() {
+  removeAllChildren(quizCardsDiv)
+
+  const quizes = JSON.parse(localStorage.getItem('Quiz Bank'))
+  quizes ?
+    quizes.map(quiz => {
+      quizCardsDiv.innerHTML += `
+        <div class="quiz_card">
+          <div class="box">
+            <div class="content">
+              <h2>${quiz.index + 1}</h2>
+              <h3>Quiz ${quiz.index + 1}</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#" onclick="takeQuiz(${quiz.index})">Take this quiz</a>
+            </div>
+          </div>
+        </div>
+      `
+    }) :
+    quizCardsDiv.innerHTML += `<p>There is no available quiz to take now!</p>`
+}
 
 // Main
 
@@ -435,4 +412,4 @@ const questions = []
 const userAnswers = []
 let userScore = 0
 
-renderQuizesSection()
+renderQuizCards()
